9a04df03b0737b15c8d74b8c0fd60ddf61ef4e7f
Lucane <freezingicecold@gmail.com>
2023-10-09 00:35:29 +0300

    Fixed authentication issues for the Flex sales portal by fetching new tokens for every API call. Also cleaned up some of the code and added XML method comments.

--- Parts/Flex/ApiCaller.cs
+++ Parts/Flex/ApiCaller.cs
@@ -1,20 +1,28 @@
 ﻿using System.Text;
 using System.Text.Json;
-using Stocker.Parts.Flex;
+using System.Text.RegularExpressions;
 
 namespace Stocker.Parts.Flex;
 
 
 public class ApiCaller
 {
+    /// <summary>
+    /// Sends a request to the Flex sales portal to retrieve the prices and stock availability for the provided part numbers.
+    /// </summary>
+    /// <param name="partNumbers">List of part number SKUs.</param>
+    /// <returns>An object that contains the sanitized and deserialized JSON data.</returns>
     public static async Task<List<StockObject>?> GetStockStatus(List<string> partNumbers)
     {
-        var response_stocks = await RequestStocks(partNumbers);
-        var response_prices = await RequestPrices(partNumbers);
+        var response_auth = await RequestAuthTokens();
+        ApiRemotingImplObject? object_auth = await ParseAuthTokens(response_auth);
+
+        var response_stocks = await RequestStocks(partNumbers, object_auth);
+        var response_prices = await RequestPrices(partNumbers, object_auth);
 
         if (response_stocks is null || response_prices is null) return null;
 
-        return await DeserializeAndCombineRequests(response_stocks, response_prices);
+        return await DeserializeAndCombineResults(response_stocks, response_prices);
 
         //var populateResponse = await PopulateDatabase(apiResponse);
         //if (populateResponse is false) return null;
@@ -22,12 +30,15 @@ public class ApiCaller
         //return await Task.FromResult(true);
     }
 
-    public static async Task<List<StockObject>?> DeserializeAndCombineRequests(string stockContent, string priceContent)
+    /// <summary>
+    /// Deserializes and combines the two separate plaintext JSON datasets.
+    /// </summary>
+    /// <param name="stockContent"></param>
+    /// <param name="priceContent"></param>
+    /// <returns>An object that contains the deserialized JSON data.</returns>
+    public static async Task<List<StockObject>?> DeserializeAndCombineResults(string stockContent, string priceContent)
     {
         if (string.IsNullOrWhiteSpace(stockContent)) return null;
-
-        //stockContent = stockContent.Trim('[').Trim(']');
-        //priceContent = priceContent.Trim('[').Trim(']');
 
         ApiStockObject? object_stocks = null;
         ApiPriceObject? object_prices = null;
@@ -48,7 +59,7 @@ public class ApiCaller
         foreach (var product in object_stocks.result.data.InvWarehouse) {
             var partNo = product.CC_Product__r.ccrz__SKU__c;
             var price = object_prices.result.data.Where(x => x.prodBean.SKU == partNo).Select(x => x.price).FirstOrDefault();
-            price = Decimal.Round(price * (decimal)0.9, 2);      // as a Lenovo reseller & partner, we get a 10% discount
+            price = Decimal.Round(price * (decimal)0.9, 2);      // As a Lenovo reseller & partner, we get a 10% discount.
 
             DateTime date = new DateTime();
 
@@ -96,60 +107,44 @@ public class ApiCaller
         return allProducts;
     }
 
-    public static async Task<string?> RequestStocks(List<string> partNumbers)
+    /// <summary>
+    /// Sends a POST request to fetch the JSON data containing the stock availability for the provided part numbers.
+    /// </summary>
+    /// <param name="partNumbers"></param>
+    /// <param name="authTokens"></param>
+    /// <returns>The web response content payload if the request was successful.</returns>
+    public static async Task<string?> RequestStocks(List<string> partNumbers, ApiRemotingImplObject authTokens)
     {
-        /*{"data":[{
-                                "storefront":"LenovoEsales",
-              "userIsoCode":"EUR",
-              "userLocale":"en_US",
-              "currentPageName":"ccrz_Products"},
-                     ["5CB0Z69532", "5CB0Z69533"]
-                    ],
-            "tid":13,
-            "ctx":{
-                                "csrf":"VmpFPSxNakF5TWkwd09DMHlNRlF3T0RveE16b3lNUzQxTmpoYSxDYzRKTFZTWVdOX24wZHZuWThUc3hnLE1HTXpNVE15",
-                  "ns":"",
-                  "ver":41,
-                  "vid":"066a0000000CqD0"},
-            "type":"rpc",
-            "action":"cc_lenovo_ctrl_ProductListShowProduct",
-            "method":"InventoryBy_Warehouse"
-        }*/
-
+        var vid = authTokens.vf.vid;
+        var tokenIe = authTokens.actions.productListShowProduct.ms.Where(ms => ms.name == "InventoryBy_Warehouse").First();
+        var ns = tokenIe.ns;
+        var ver = tokenIe.ver;
+        var csrfToken = tokenIe.csrf;
+        var authToken = tokenIe.authorization;
 
         var client = new HttpClient();
         client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
-        client.DefaultRequestHeaders.Add("referer", @"https://www.lenovopartsales.com/LenovoEsales/ccrz__Products?cartID=&portalUser=&store=&cclcl=en_US&operation=quickSearch&searchText=t14");
-
-        //var requestObj = new RequestContentObj();
-        //requestObj.data.currentPageName = 
-        //request.
-
-        //requestObj.ctx.csrf = "VmpFPSxNakF5TWkwd09DMHlNRlF3T0RveE16b3lNUzQxTmpoYSxDYzRKTFZTWVdOX24wZHZuWThUc3hnLE1HTXpNVE15";
-        //requestObj.ctx.ns = "";
-        //requestObj.ctx.ver = 41;
-        //requestObj.ctx.vid = "066a0000000CqD0";
+        client.DefaultRequestHeaders.Add("referer", @"https://www.lenovopartsales.com/LenovoEsales/ccrz__HomePage");
 
         HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://www.lenovopartsales.com/LenovoEsales/apexremote");
-        request.Content = new StringContent(@"{""data"":[{
-                                                      ""storefront"":""LenovoEsales"",
-                                                      ""userIsoCode"":""EUR"",
-                                                      ""userLocale"":""en_US"",
-                                                      ""currentPageName"":""ccrz_Products""}," +
-                                                         JsonSerializer.Serialize(partNumbers) +
-                                                        @"],
-                                                        ""tid"":11,
-                                                        ""ctx"":{""csrf"":""VmpFPSxNakF5TWkwd09DMHlNVlF4T0RveU5EbzFPUzQ0TURaYSxDN283N0tvT0IxaW95N2ZJV1pPWXIwLE1HTXpNVE16"",
-	                                                            ""ns"":"""",
-	                                                            ""ver"":41,
-	                                                            ""vid"":""066a0000000CqD0""},
-                                                        ""type"":""rpc"",
-                                                        ""action"":""cc_lenovo_ctrl_ProductListShowProduct"",
-                                                        ""method"":""InventoryBy_Warehouse""}",
+        request.Content = new StringContent($@"{{ ""action"": ""cc_lenovo_ctrl_ProductListShowProduct"",
+                                                ""method"": ""InventoryBy_Warehouse"",
+                                                ""type"": ""rpc"",
+                                                ""tid"": 13,
+                                                ""data"": [{{}},
+                                                    {JsonSerializer.Serialize(partNumbers)}
+                                                ],
+                                                ""ctx"": {{
+                                                    ""authorization"": ""{authToken}"",
+                                                    ""csrf"": ""{csrfToken}"",
+                                                    ""vid"": ""{vid}"",
+                                                    ""ns"": ""{ns}"",
+                                                    ""ver"": {ver}
+                                                }},
+                                                
+                                            }}",
                                             Encoding.UTF8,
                                             "application/json");
-
-        //System.Diagnostics.Debug.WriteLine($@"request content :: {await request.Content.ReadAsStringAsync()}");
 
         try {
             var response = await client.SendAsync(request);
@@ -163,37 +158,46 @@ public class ApiCaller
         return null;
     }
 
-    public static async Task<string?> RequestPrices(List<string> partNumbers)
+    /// <summary>
+    /// Sends a POST request to fetch the JSON data containing the prices for the provided part numbers.
+    /// </summary>
+    /// <param name="partNumbers"></param>
+    /// <param name="authTokens"></param>
+    /// <returns>The web response content payload if the request was successful.</returns>
+    public static async Task<string?> RequestPrices(List<string> partNumbers, ApiRemotingImplObject authTokens)
     {
         var serializedSKUs = "";
 
         foreach (var p in partNumbers) {
             serializedSKUs += @"{""sku"": """ + p + @"""},";
         }
+        serializedSKUs = serializedSKUs.TrimEnd(',');      // Removes the extra comma that was added in the foreach loop.
 
-        serializedSKUs = serializedSKUs.TrimEnd(',');      // removes the extra comma that was added in the foreach loop
+        var vid = authTokens.vf.vid;
+        var tokenIe = authTokens.actions.productListRd.ms.Where(ms => ms.name == "fetchPage").First();
+        var ns = tokenIe.ns;
+        var ver = tokenIe.ver;
+        var csrfToken = tokenIe.csrf;
+        var authToken = tokenIe.authorization;
 
         var client = new HttpClient();
         client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
-        client.DefaultRequestHeaders.Add("referer", @"https://www.lenovopartsales.com/LenovoEsales/ccrz__Products?cartID=&portalUser=&store=&cclcl=en_US&operation=quickSearch&searchText=t14");
+        client.DefaultRequestHeaders.Add("referer", @"https://www.lenovopartsales.com/LenovoEsales/ccrz__HomePage");
 
         HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://www.lenovopartsales.com/LenovoEsales/apexremote");
-        request.Content = new StringContent(@"{ ""action"": ""ccrz.cc_ctrl_ProductListRD"",
+        request.Content = new StringContent($@"{{ ""action"": ""ccrz.cc_ctrl_ProductListRD"",
                                                 ""method"": ""fetchPage"",
-                                                ""data"": [{},
-                                                    ["
-                                                        + serializedSKUs +
-                                                  @"]
-                                                ],
                                                 ""type"": ""rpc"",
                                                 ""tid"": 10,
-                                                ""ctx"": {
-                                                    ""csrf"": ""VmpFPSxNakF5TWkwd09DMHlNVlF5TURveU56bzBNeTQwTkRKYSxQSWtZa19nbGVqTGlNdHBoVGh6aTRnLE5HRTJNamcz"",
-                                                    ""vid"": ""066a0000000CqD0"",
-                                                    ""ns"": ""ccrz"",
-                                                    ""ver"": 50
-                                                }
-                                            }",
+                                                ""data"": [{{}}, [{serializedSKUs}]],
+                                                ""ctx"": {{
+                                                    ""authorization"": ""{authToken}"",
+                                                    ""csrf"": ""{csrfToken}"",
+                                                    ""vid"": ""{vid}"",
+                                                    ""ns"": ""{ns}"",
+                                                    ""ver"": {ver}
+                                                }}
+                                            }}",
                                             Encoding.UTF8,
                                             "application/json");
 
@@ -206,5 +210,51 @@ public class ApiCaller
         }
 
         return null;
+    }
+
+    /// <summary>
+    /// Sends a GET request to fetch the CSRF and auth tokens for the Flex sales portal.
+    /// </summary>
+    /// <returns>The web response content payload if the request was successful.</returns>
+    /// <remarks>Since the API endpoint is protected, tokens are required to send POST requests.</remarks>
+    public static async Task<string?> RequestAuthTokens()
+    {
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
+        client.DefaultRequestHeaders.Add("referer", @"https://www.lenovopartsales.com/LenovoEsales/ccrz__HomePage");
+
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://www.lenovopartsales.com/LenovoEsales/ccrz__Products");
+
+        try {
+            var response = await client.SendAsync(request);
+            System.Diagnostics.Debug.WriteLine($@"Response from Flex price query :: {response}");
+            if (response is not null && response.IsSuccessStatusCode) return await response.Content.ReadAsStringAsync();
+        } catch (Exception ex) {
+            System.Diagnostics.Debug.WriteLine($@"ERROR while performing Flex price query :: {ex.Message}{Environment.NewLine}{ex.InnerException}");
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Sanitizes the returned JSON data with RegEx and then deserializes/objectifies the data.
+    /// </summary>
+    /// <param name="authContent"></param>
+    /// <returns>An object that contains the sanitized and deserialized JSON data.</returns>
+    public static async Task<ApiRemotingImplObject?> ParseAuthTokens(string authContent)
+    {
+        var authJson = Regex.Match(authContent, @"VFRM\.RemotingProviderImpl\(((.|\n)*?)\)\);").Groups[1].Value;
+
+        ApiRemotingImplObject? object_auth = null;
+
+        try {
+            object_auth = JsonSerializer.Deserialize<ApiRemotingImplObject>(authJson);
+
+        } catch (Exception ex) {
+            System.Diagnostics.Debug.WriteLine($@"ERROR while deserializing response from Flex :: {ex.Message}{Environment.NewLine}{ex.InnerException}");
+            return null;
+        }
+
+        return object_auth;
     }
 }
---
+++ Parts/Flex/ApiRemotingImplObject.cs
@@ -0,0 +1,125 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Stocker.Parts.Flex;
+
+
+public class ApiRemotingImplObject
+{
+    public Vf vf { get; set; }
+    public Actions actions { get; set; }
+    public string service { get; set; }
+}
+
+public class Vf
+{
+    public string vid { get; set; }
+    public bool xhr { get; set; }
+    public bool dev { get; set; }
+    public bool tst { get; set; }
+    public bool dbg { get; set; }
+    public long tm { get; set; }
+    public bool ovrprm { get; set; }
+}
+
+public class Actions
+{
+    [JsonPropertyName("cc_lenovo_ctrl_ProductListShowProduct")]
+    public ProductListShowProduct productListShowProduct { get; set; }
+
+    [JsonPropertyName("cc_lenovo_ctrl_QuickOrderAlternatives")]
+    public QuickOrderAlternatives quickOrderAlternatives { get; set; }
+
+    [JsonPropertyName("ccrz.cc_ctrl_BreadCrumb")]
+    public BreadCrumb breadCrumb { get; set; }
+
+    [JsonPropertyName("ccrz.cc_ctrl_CartRD")]
+    public CartRd cartRd { get; set; }
+
+    [JsonPropertyName("ccrz.cc_ctrl_FeatureFilterRD")]
+    public FeatureFilterRd featureFilterRd { get; set; }
+
+    [JsonPropertyName("ccrz.cc_ctrl_Header")]
+    public Header header { get; set; }
+
+    [JsonPropertyName("ccrz.cc_ctrl_MenuBar")]
+    public MenuBar menuBar { get; set; }
+
+    [JsonPropertyName("ccrz.cc_ctrl_ProductListRD")]
+    public ProductListRd productListRd { get; set; }
+
+    [JsonPropertyName("ccrz.cc_ctrl_promoRD")]
+    public PromoRd promoRD { get; set; }
+
+    [JsonPropertyName("ccrz.cc_RemoteActionController")]
+    public RemoteActionController remoteActionController { get; set; }
+}
+
+public class M
+{
+    public string name { get; set; }
+    public int len { get; set; }
+    public string ns { get; set; }
+    public float ver { get; set; }
+    public string csrf { get; set; }
+    public string authorization { get; set; }
+}
+
+public class ProductListShowProduct
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class QuickOrderAlternatives
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class BreadCrumb
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class CartRd
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class FeatureFilterRd
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class Header
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class MenuBar
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class ProductListRd
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class PromoRd
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
+
+public class RemoteActionController
+{
+    public M[] ms { get; set; }
+    public int prm { get; set; }
+}
